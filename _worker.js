82716f15f89b37288cd304a6cabea70c2950c5e8
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-09-04 04:08:51 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,14 +1,14 @@
 export default {
-  async fetch(request) {
+  async fetch(request, env) {
     const url = new URL(request.url);
     if (url.pathname.endsWith(".zip")) {
-      const target = `https://github.com/bibicadotnet/microsoft-edge-multi-portable/releases/download${url.pathname}${url.search}`;
+      const target = https://github.com/bibicadotnet/microsoft-edge-multi-portable/releases/download${url.pathname}${url.search};
       return fetch(target, {
         method: request.method,
         headers: request.headers,
         redirect: "follow",
       });
     }
-    return new Response("Not found", { status: 404 });
+    return env.ASSETS.fetch(request);
   }
 };
