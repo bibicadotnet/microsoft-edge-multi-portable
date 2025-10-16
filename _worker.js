02553ab0092a259551dcdaa4f8206ce541500fe3
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-10-16 21:08:20 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
-    if (url.pathname.endsWith(".zip")) {
+    if (url.pathname.endsWith(".zip" ".json")) {
       const target = `https://github.com/bibicadotnet/microsoft-edge-multi-portable/releases/download${url.pathname}${url.search}`;
 
       return fetch(target, {
