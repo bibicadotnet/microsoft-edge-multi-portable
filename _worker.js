81082888ea4a34b5a93bd5e0b24b1bc8ca23dc21
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-10-16 21:10:11 +0700

    Fix conditional check for .zip and .json paths

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
-    if (url.pathname.endsWith(".zip" ".json")) {
+    if (url.pathname.endsWith(".zip") || url.pathname.endsWith(".json")) {
       const target = `https://github.com/bibicadotnet/microsoft-edge-multi-portable/releases/download${url.pathname}${url.search}`;
 
       return fetch(target, {
